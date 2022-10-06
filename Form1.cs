19f435abb2b0b936394aba08f718946cc48bc49b
brenekv.03 <brenekv.03@A321-15>
2022-10-06 11:23:41 +0200

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace souboryBin03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"..\..celaCisla.dat", FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs);
+            BinaryWriter bw = new BinaryWriter(fs);
+            int max = br.ReadInt32();
+            int min = br.ReadInt32();
+            int maxPosition = 0;
+            while(fs.Position<fs.Length)
+            {
+                int cislo = br.ReadInt32();
+                listBox1.Items.Add(cislo);
+                if(cislo<min)
+                {
+                    min = cislo;
+                }
+                if(cislo>max)
+                {
+                    max = cislo;
+                }
+            }
+        }
+    }
+}
